44bb2c9e69268fd6b831137130729e0fb664e82c
Emix200 <125465757+Emix200@users.noreply.github.com>
2023-04-25 07:49:43 -0700

    Update functions1.c

--- functions1.c
+++ functions1.c
@@ -3,11 +3,11 @@
 /************************* PRINT UNSIGNED NUMBER *************************/
 /**
  * print_unsigned - Prints an unsigned number
- * @types: List a of arguments
- * @buffer: Buffer array to handle print
+ * @types: List of arguments
+ * @buffer: Buffer of array to handle print
  * @flags:  Calculates active flags
  * @width: get width
- * @precision: Precision specification
+ * @precision: Precision specifications
  * @size: Size specifier
  * Return: Number of chars printed.
  */
